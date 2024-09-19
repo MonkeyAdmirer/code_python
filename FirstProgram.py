f63ecdaf05a1f02523fbdc3345ba62541456ee1d
MonkeyAdmirer <pandelistacoking@gmail.com>
2024-09-19 09:34:04 -0400

    HOLY F I MADE A CALCULATOR GET THE CAMERA!

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,1 +1,8 @@
-print("Hello, world!")
+# Basic Calculator
+
+print("Hey Fam, welcome to my bussin Calculator!")
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+answer = num1 + num2
+print(f"Your answer is: {answer}!!!")
