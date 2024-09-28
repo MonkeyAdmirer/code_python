4b6aa3d468327d9222204c953111cc253957352d
MonkeyAdmirer <pandelistacoking@gmail.com>
2024-09-28 18:30:01 -0400

    program initialized

---
+++ Week_3/Week_3_Exercise_Set_1/question1.py
@@ -0,0 +1,11 @@
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+birthYear = int(input("Enter your birth year: "))
+
+print(f"Hello {firstName}  {lastName}.")
+print(f"You are {2024 - birthYear} years old.")
+
+if birthYear >= 2005
+    print("You are not old enough to drink.")
+else
+    print("You are old enough to drink.")
