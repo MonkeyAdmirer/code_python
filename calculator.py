e485a8bf75d9e990e95f2612ce96b56021d69110
MonkeyAdmirer <pandelistacoking@gmail.com>
2024-09-28 17:22:07 -0400

    finished program

---
+++ calculator.py
@@ -0,0 +1,21 @@
+print("Welcome to our calculator app!")
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+choice = input("Choose either: add, subtract, multiply, or divide: ")
+
+if choice == "add":
+    print(f"{num1} + {num2} = {num1 + num2}")
+
+elif choice == "subtract":
+    print(f"{num1} - {num2} = {num1 - num2}")
+
+elif choice == "multiply":
+    print(f"{num1} * {num2} = {num1 * num2}")
+
+elif choice == "divide":
+    print(f"{num1} / {num2} = {num1 / num2}")
+
+else:
+    print("Wrong input")
