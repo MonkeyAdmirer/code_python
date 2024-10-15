0aa906ab212679ed39be2342ddfc0b39cecca676
MonkeyAdmirer <pandelistacoking@gmail.com>
2024-10-15 09:14:47 -0400

    Intializing python program

---
+++ Week_4/Week_4_Questions/palindrome_checker.py
@@ -0,0 +1,8 @@
+def isPalindrome(word : str) -> bool:
+        if not word: # if len(word) == 0:
+            return True
+        elif len(word) == 1:
+            return True
+        elif len(word) <= 3"
+            return word[0] == word [-1]
+        else:
