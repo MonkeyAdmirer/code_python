507263aef991a5335ca2abde7397d22e31aec1a9
MonkeyAdmirer <pandelistacoking@gmail.com>
2024-11-04 09:40:06 -0500

    intializing python program

---
+++ chores.py
@@ -0,0 +1,5 @@
+time = int(input("How much time do you have?"))
+num_chores = int(input("How many chores do you have to do?"))
+time_chores = []
+for i in range(num_chores):
+    time_chores.append(int(input("How much time will this chore take?")))
